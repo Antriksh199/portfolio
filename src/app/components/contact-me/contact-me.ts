c875d3a0f47349701bb6b874e74ddb4d9cd9f83a
Antriksh <antriksh199@gmail.com>
2026-02-01 19:36:36 +0530

    Copy To Clipboad Changes

--- src/app/components/contact-me/contact-me.ts
+++ src/app/components/contact-me/contact-me.ts
@@ -1,16 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
   templateUrl: './contact-me.html',
   styleUrl: './contact-me.scss',
+  imports: [CommonModule]
 })
 export class ContactMe {
 
   fullName = 'Antriksh Pandey';
   email = 'antriksh199@gmail.com';
   phone = '+918130907556';
+
   address = {
     line1: '320, Bargaon',
     city: 'Gonda',
@@ -18,15 +20,21 @@ export class ContactMe {
     country: 'India',
     zip: '271002'
   };
+
   linkedIn = 'https://www.linkedin.com/in/antriksh-pandey-80689229/';
   github = 'https://github.com/Antriksh199';
 
+  copiedField: string | null = null;
 
-  copyToClipboard(value: string) {
+  copyToClipboard(value: string, field: string) {
     navigator.clipboard.writeText(value).then(() => {
-      alert(`${value} copied to clipboard!`);
+      this.copiedField = field;
+
+      setTimeout(() => {
+        this.copiedField = null;
+      }, 1500);
+    }).catch(err => {
+      console.error('Failed to copy:', err);
     });
   }
-
-
 }
